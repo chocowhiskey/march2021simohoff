640816da164bef574ba8aefa55b84f1d7bfdc21f
Simone Hoffmann <simohoff@gmx.de>
2021-03-23 13:16:20 +0100

    Centered background in the middle of page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import background from "./images/ocean.jpg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={background} className="App-logo" alt="logo" />
-      </header>
+      <div className="App-div">
+        <div className="App-logo" alt="logo" />
 
-      <div class="center">
-        <p>This div is vertically centered.</p>
+        <p className="centeredTitle">Simone Hoffmann</p>
       </div>
     </div>
   );
